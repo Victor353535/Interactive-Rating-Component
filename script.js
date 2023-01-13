a10ba1828deab560aaa475f2e760f335cb7c3127
Victor353535 <87070090+Victor353535@users.noreply.github.com>
2023-01-13 17:57:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,26 @@
 // Variables
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
-const four = document.getElementById('four');
-const five = document.getElementById('five');
-const numberSelection = document.getElementById('number-selection');
 
+const numbers = document.querySelectorAll('.number');
 const submit = document.getElementById('submit');
 const card1 = document.getElementById('rating-state')
 const card2 = document.getElementById('thank-you-state')
+const numberSelection = document.getElementById('number-selection');
 
-const rate_number = []
+let rateNumber = 0;
 
+// Event LIsteners
 
-
-// Event Listeners
-
-one.addEventListener('click', () => {
-    rate_number.push(one.textContent);
-    numberSelection.innerText = `You selected ${rate_number} out of 5`
-});
-
-two.addEventListener('click', () => {
-    rate_number.push(two.textContent);
-    numberSelection.innerText = `You selected ${rate_number} out of 5`
-});
-
-three.addEventListener('click', () => {
-    rate_number.push(three.textContent);
-    numberSelection.innerText = `You selected ${rate_number} out of 5`
-});
-
-
-four.addEventListener('click', () => {
-    rate_number.push(four.textContent);
-    numberSelection.innerText = `You selected ${rate_number} out of 5`
-});
-
-five.addEventListener('click', () => {
-    rate_number.push(five.textContent);
-    numberSelection.innerText = `You selected ${rate_number} out of 5`
+numbers.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        rateNumber = e.target.textContent
+        numberSelection.innerHTML = `You selected ${rateNumber} out of 5`
+    });
 });
 
 submit.addEventListener('click', (e) => {
     e.preventDefault();
-    card1.style.display = 'none';
-    card2.style.display = 'block';
+    if (rateNumber !== 0) {
+        card1.style.display = 'none'
+        card2.style.display = 'block'
+    }
 });
-
